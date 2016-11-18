e38b2e35ddb0d91bc83f48ee2bc726f7e72ed20d
Shajed <shajedul.hoque@rootnext.com>
2016-11-18 19:57:36 +0600

    append data infinitely

--- assets/js/application.js
+++ assets/js/application.js
@@ -13,12 +13,37 @@ $(document).ready(function (){
   pageRender('app/home.html');
   link = document.location.href;
   window.history.replaceState(null, null, link.split('.html')[0]);
+
+  var notice = ['one', 'two', 'three', 'four', 'five'];
+  var i = 0;
+
+  function myLoop () {
+    setTimeout(function () {
+      var viewNotice = $('<span>'+ notice[i] + '</span> <br />');
+      $('div#notice').html(viewNotice);
+      i++;
+      if (i == 5) {
+        i = 0;
+      }
+      myLoop();
+    }, 1000)
+  }
+  myLoop();
 });
 
 $('a.app-content').click(function(){
   var page =  $(this).data('content');
   var path = 'app/'+ page +'.html';
   pageRender(path);
+
+  //console.log(page);
+  setTimeout(function(){
+    if(page == 'home'){
+      console.log(page);
+      var content = $('<h1>Welcome to our ' + page + ' page</h1>');
+      $("div#main-body").prepend(content);
+    }
+  }, 300);
 });
 
 function pageRender(page){
